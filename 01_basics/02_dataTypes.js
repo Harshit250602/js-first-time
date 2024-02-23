3aae7561fb1cd68f1fdfdd65e28217b8ba9fce11
Harshit250602 <127775062+Harshit250602@users.noreply.github.com>
2024-02-23 20:27:14 +0000

    2nd manual update

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,7 @@
+"use strict"; // treat all JS code as newer version
+//alert(3+3) if you run this in console of chrome browser u will get 6 but in nodejs you have different syntax for it
+//null is a standalone value in javascript
+//if u check datatype of null it will be object while undefined is itself a datatype
+let age =19
+console.log(typeof "panther")
+console.log(typeof age)
