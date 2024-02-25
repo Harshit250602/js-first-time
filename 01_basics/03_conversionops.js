e47898327af6f69575e4a3c9f7e3fda753713ba7
Harshit250602 <127775062+Harshit250602@users.noreply.github.com>
2024-02-25 16:48:52 +0000

    everything done

--- 01_basics/03_conversionops.js
+++ 01_basics/03_conversionops.js
@@ -1,17 +1,37 @@
-let score = "33ab";
-console.log(typeof score);
+// let score = "33ab";
+// console.log(typeof score);
 
-/*
-actually sometimes you get data from somewhere and the data type of what you get is differnet
-then on what you want to perform operation so u need conversion.
-*/
+// /*
+// actually sometimes you get data from somewhere and the data type of what you get is differnet
+// then on what you want to perform operation so u need conversion.
+// */
 
-let valueInnum = Number(score)
+// let valueInnum = Number(score)
 
-console.log(valueInnum);
+// console.log(valueInnum);
 
-console.log(typeof valueInnum);
-let ip = "yo" // if "" then false 1=> true, 0=> false
+// console.log(typeof valueInnum);
+// let ip = "yo" // if "" then false 1=> true, 0=> false
 
-let given_ip = Boolean(ip)
-console.log(given_ip);
+// let given_ip = Boolean(ip)
+// console.log(given_ip);
+
+//***********Operations:  */
+
+let val = 3;
+let negval= -val
+console.log(negval)
+
+str1 ="Best player"
+str2 =" Harshit"
+str3 = str1+str2;
+console.log(str3);
+console.log("1"+2+2);
+console.log(1+2+"2");
+let x = 3;
+const y = x++;
+// x is 4; y is 3
+
+let x2 = 3n;
+const y2 = x2++;
+// x2 is 4n; y2 is 3n
