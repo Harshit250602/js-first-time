64ae9db5dc219af04437beeebe4487b0584a56e8
Harshit250602 <127775062+Harshit250602@users.noreply.github.com>
2024-03-09 19:30:48 +0000

    data type summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,8 @@
+/*categorized as :
+non primitive (refrence type) and primitve(7 do search) based on how data is stored and accessed
+java script is dynamically typed  */
+
+const id = Symbol("123")
+const anotherid = Symbol("123") // symbol is another data type
+
+console.log(id==anotherid);
